d5028bdb0f22dbe0db472895160c2f617d78d0ed
NHPT <42366434+NHPT@users.noreply.github.com>
2024-03-04 17:52:07 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -e
-set -x
 
-# Start database
-sudo service postgresql restart
+# Check if the script is run by root
+if [ "$(id -u)" -eq 0 ]; then
+    # The script is run by root, no need for sudo
+    service postgresql restart
+else
+    # The script is run by a non-root user, use sudo
+    sudo service postgresql restart
+fi
+
 echo "Waiting for DB to start..."
 sleep 5
